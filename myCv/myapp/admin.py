5b335fe3d237eac69cec75922807520730eb5209
shamshadhusain3 <111483127+shamshadhusain3@users.noreply.github.com>
2023-08-18 11:32:27 +0530

    Delete myCv directory

--- myCv/myapp/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import contact
-
-# Register your models here.
-class adminModel(admin.ModelAdmin):
-    list_display=['name', 'email', 'address']
-
-
-admin.site.register(contact,adminModel)
--- myCv/static/resp.js
+++
@@ -1,6 +0,0 @@
-let menuBtn=document.querySelector(".burger")
-let navbar=document.querySelector(".vclass")
-
-menuBtn.addEventListener('click',function(){
-  navbar.classList.toggle("active")
-})
