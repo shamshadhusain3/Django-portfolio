7c13333b83fd9e2835de18b2360410446e9576d7
shamshadhusain3 <111483127+shamshadhusain3@users.noreply.github.com>
2023-08-24 16:48:24 +0530

    Update settings.py

--- myCv/settings.py
+++ myCv/settings.py
@@ -74,7 +74,7 @@ WSGI_APPLICATION = 'myCv.wsgi.application'
 
 
 # Database
-https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = { 
     'default': {
